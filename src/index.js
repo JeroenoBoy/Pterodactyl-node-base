0b0ff3dd62b068d678e54fa4eb64105e46f933e8
JeroenoBoy <Jeroen@vandegeest.eu>
2020-09-26 16:33:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-console.log('.');
-console.log('.');
-console.log('.');
-
+console.log('ᅠᅠ');
+console.log('ᅠᅠ');
+console.log('ᅠᅠ');
 console.log('Hello World!');
 console.log('These are the files for my base pterodactyl egg.');
